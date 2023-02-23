9e277e115584298e97dd0b4b35afdd69e46a8bd9
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-02-22 22:17:41 -0300

    all css

--- portifolio/src/components/NavBar.js
+++ portifolio/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import {Navbar, Container, Nav} from 'react-bootstrap';
 import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/navIcon1.svg'
 import navIcon2 from '../assets/img/navIcon2.svg'
@@ -30,19 +30,19 @@ const onUptadeActiveLink = (value) => {
 return(<Navbar expand='lg' className={scrolled ? "scrolled": ''}>
         <Container>
             <Navbar.Brand href='#home'>
-                <img src={''} alt="logo"/>
+                <img src={logo} alt="logo"/>
             </Navbar.Brand>
-            <Navbar.toggle aria-controls='basic-navbar-nav'>
+            <Navbar.Toggle aria-controls='basic-navbar-nav'>
                 <span className='navbar-toggler-icon'></span>
-            </Navbar.toggle>
-            <Navbar.collapse id='basic-navbar-nav'>
+            </Navbar.Toggle>
+            <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='me-auto'>
-                    <Nav.link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-                    onClick={() => onUptadeActiveLink('home')}>Home</Nav.link>
-                    <Nav.link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-                    onClick={() => onUptadeActiveLink('skills')}>Skills</Nav.link>
-                    <Nav.link href='#Projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
-                    onClick={() => onUptadeActiveLink('projects')}>Projects</Nav.link>
+                    <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
+                    onClick={() => onUptadeActiveLink('home')}>Home</Nav.Link>
+                    <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
+                    onClick={() => onUptadeActiveLink('skills')}>Skills</Nav.Link>
+                    <Nav.Link href='#Projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
+                    onClick={() => onUptadeActiveLink('projects')}>Projects</Nav.Link>
                 </Nav>
                 <span className='navbar-text'>
                     <div className='social-icon'>
@@ -54,7 +54,7 @@ return(<Navbar expand='lg' className={scrolled ? "scrolled": ''}>
                         <span>Let's Connect</span>
                     </button>
                 </span>
-            </Navbar.collapse>
+            </Navbar.Collapse>
         </Container>
     </Navbar>)
 }
